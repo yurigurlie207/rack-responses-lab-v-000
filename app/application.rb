26e59519bf7cc181a46bebcdf893c1b042215e7e
Adrienne Yao <adrienne.yao@icloud.com>
2019-08-22 02:01:49 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    time = Kernel.rand(1..20)
+
+    if num_1==num_2 && num_2==num_3
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+    end
+
+    resp.finish
+  end
+
+end
